42485cd482737b6045f79a837351c2127e87cced
Seunghyeon Ban <sban6@my.centennialcollege.ca>
2024-02-24 19:32:44 -0500

    LAb2 webapp

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f4/70ddd8f150d3001e105eeb24ecbadcde
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to comp 367!";
+    }
+}
+
---
+++ lab2-webapp/src/main/java/com/example/demo/Lab2WebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2WebappApplication.class, args);
+	}
+
+}
